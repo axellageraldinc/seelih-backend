f04277f27efbb56462337d643b154fd62d4ac85d
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2018-11-21 09:24:25 +0700

    [ADD] endpoint `/api/register` for user registration

--- app/controller/UserController.go
+++ app/controller/UserController.go
@@ -12,6 +12,39 @@ import (
 	"net/http"
 )
 
+func Register(w http.ResponseWriter, r *http.Request) {
+	golog.Info("/api/register")
+
+	db := helper.OpenDatabaseConnection()
+	defer db.Close()
+
+	var registerRequest RegisterRequest
+	var user User
+	var response WebResponse
+
+	json.NewDecoder(r.Body).Decode(&registerRequest)
+
+	if db.Where("email = ?", registerRequest.Email).Find(&user).RecordNotFound() {
+		plainPasswordInByte := convertPlainPasswordToByte(registerRequest.Password)
+		hashedPassword := hashAndSalt(plainPasswordInByte)
+		user = User{
+			Email: registerRequest.Email,
+			Password: hashedPassword,
+			CityCodeId: registerRequest.CityCode,
+			Fullname: registerRequest.Fullname,
+			Fulladdress: registerRequest.FullAddress,
+			Phone: registerRequest.Phone,
+		}
+		db.Create(&user)
+		response = OK(nil)
+		golog.Info("User registration succeed")
+	} else {
+		golog.Warn("User with email " + registerRequest.Email + " already exists in DB")
+		response = ERROR(REGISTER_FAILED_EMAIL_ALREADY_EXISTS)
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
 	golog.Info("/api/login")
 
@@ -41,6 +74,21 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(response)
+}
+
+func hashAndSalt(bytePlainPassword []byte) string {
+	// Use GenerateFromPassword to hash & salt bytePlainPassword
+	// MinCost is just an integer constant provided by the bcrypt
+	// package along with DefaultCost & MaxCost.
+	// The cost can be any value you want provided it isn't lower
+	// than the MinCost (4)
+	hash, err := bcrypt.GenerateFromPassword(bytePlainPassword, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	// GenerateFromPassword returns a byte slice so we need to
+	// convert the bytes to a string and return it
+	return string(hash)
 }
 
 func convertPlainPasswordToByte(plainPassword string) []byte {
